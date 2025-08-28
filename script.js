7dc56a02320cc5025ebd476157aca0aa8c4424b2
Ssayi91 <sonny.sayi@gmail.com>
2025-08-28 19:04:30 +0300

    new entry

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    // const par = document.getElementById('parallax');
+    // function onPointer(e) {
+    //   const x = (e.clientX / window.innerWidth - 0.5) * 2;
+    //   const y = (e.clientY / window.innerHeight - 0.5) * 2;
+    //   par.style.transform = `translate(${x * 30}px, ${y * 30}px)`;
+    // }
+    window.addEventListener('mousemove', onPointer);
